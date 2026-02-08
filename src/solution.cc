9a08436b03c9afc6c1ba3b87dd526b01555bb38e
Yue-567 <yuey33@illinois.edu>
2026-02-08 14:57:31 -0600

    2.9 homework

--- src/solution.cc
+++ src/solution.cc
@@ -2,5 +2,32 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
-  return Student{};
+  std::string first_name, last_name;
+  unsigned int uin;
+  double gpa 0.0;
+  char comma1 = '\0', comma2 = '\0';
+  if (!(is >> first_name >> last_name)) {
+    return Student{};
+  }
+  if (last_name.empty() ||last_name.back() != ',') {
+    return Student{};
+  }
+  last_name.pop.back();
+  if (!(is >> comma1) || comma1 != ',') {
+    return Student{};
+  }
+  if (!(uin)) {
+    return Student{};
+  }
+  if (!(is >> comma2) || comma2 != ',') {
+    return Student{};
+  }
+  if (!(is >> gpa)) {
+    return Student{};
+  }
+  Student s;
+  s.full_name = first_name + " " + last_name;
+  s.uin = uin;
+  s.gpa = gpa;
+  return s;
 }
