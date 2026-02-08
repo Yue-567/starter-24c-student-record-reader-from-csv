31f96777849470d84779854f30e7f91ad3854957
Yue-567 <yuey33@illinois.edu>
2026-02-08 15:04:49 -0600

    9.2 hw1

--- src/solution.cc
+++ src/solution.cc
@@ -3,20 +3,20 @@
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
   std::string first_name, last_name;
-  unsigned int uin;
+  unsigned int uin = 0;
   double gpa = 0.0;
   char comma1 = '\0', comma2 = '\0';
   if (!(is >> first_name >> last_name)) {
     return Student{};
   }
-  if (last_name.empty() ||last_name.back() != ',') {
+  if (last_name.empty() || last_name.back() != ',') {
     return Student{};
   }
   last_name.pop_back();
   if (!(is >> comma1) || comma1 != ',') {
     return Student{};
   }
-  if (!(uin)) {
+  if (!(is >> uin)) {
     return Student{};
   }
   if (!(is >> comma2) || comma2 != ',') {
